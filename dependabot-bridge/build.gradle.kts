f89c27ccc3616de357df50d81317de005d967ed4
RBusarow <RBusarow@users.noreply.github.com>
2021-10-12 06:02:25 +0000

    sort dependencies
    Signed-off-by: GitHub Actions <actions@github.com>

--- dependabot-bridge/build.gradle.kts
+++ dependabot-bridge/build.gradle.kts
@@ -245,6 +245,7 @@ dependencies {
 
 
 
+
   dependencySync("com.android.tools.build:gradle:7.0.2")
   dependencySync("com.github.javaparser:javaparser-symbol-solver-core:3.23.0")
   dependencySync("com.github.tschuchortdev:kotlin-compile-testing:1.4.5")
@@ -287,6 +288,7 @@ dependencies {
   dependencySync("com.squareup.retrofit2:retrofit:2.9.0")
   dependencySync("com.squareup:kotlinpoet:1.10.1")
   dependencySync("com.vanniktech:gradle-maven-publish-plugin:0.18.0")
+
 
 
 
@@ -624,7 +626,9 @@ dependencies {
 
 
 
+
   dependencySync("gradle.plugin.dev.arunkumar:scabbard-gradle-plugin:0.5.0")
+
 
 
 
@@ -966,8 +970,10 @@ dependencies {
 
 
 
+
   dependencySync("javax.annotation:jsr250-api:1.0")
   dependencySync("javax.inject:javax.inject:1")
+
 
 
 
@@ -1301,7 +1307,9 @@ dependencies {
 
 
 
+
   dependencySync("net.swiftzer.semver:semver:1.1.1")
+
 
 
 
